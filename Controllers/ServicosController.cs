d99118ca3d1478c27741943ee4a9db8b1d9465a2
Luiz Otávio <134003535+S-accarose@users.noreply.github.com>
2025-08-17 08:53:50 -0300

    Mudança Função do controller e adição dos cards

--- Controllers/ServicosController.cs
+++ Controllers/ServicosController.cs
@@ -22,9 +22,9 @@ namespace DSEB_projeto.Controllers
         // GET: Servicos
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Servicos.ToListAsync());
+            var servicos = await _context.Servicos.ToListAsync();
+            return View(servicos);
         }
-
         // GET: Servicos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -154,4 +154,5 @@ namespace DSEB_projeto.Controllers
             return _context.Servicos.Any(e => e.Id == id);
         }
     }
+
 }
