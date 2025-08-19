aff6973396e09600ac9c1e37c2ac48fdaf09f3cb
eovittinho <victorthulio001@gmail.com>
2025-08-19 11:04:50 -0300

    Add images

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -10,6 +10,7 @@ public class Pedido
     public string? Descricao { get; set; }
     public decimal Valor { get; set; }
     public DateTime DataPedido { get; set; }
+    public DateTime DataEntrega { get; set; }
     // Relação com o usuário que fez o pedido
     [Required]
     public string UsuarioId { get; set; }
